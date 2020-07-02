37be4af9a14f3d4046f680f84504f43285971c96
txkedown <txkedownlol@gmail.com>
2020-07-02 12:53:54 +1000

    asdasdasdasdasdasdasd

--- index.js
+++ index.js
@@ -2,38 +2,76 @@ const botconfig = require("./botconfig.json");
 const colours = require("./colours.json");
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const moment = require("moment");
+require("moment-duration-format");
 
 bot.on('ready', async () =>{
     console.log('This bot is online!')
     bot.user.setActivity("v.1.0.0", {type: "PLAYING"});
 })
 
-bot.on("message", async message => {
-    if(message.author.bot || message.channel.type === "dm") return;
+bot.on("message", message => {
+    let prefix = "=";
+    if (message.author.bot || !message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
 
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(" ")
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
+// Ping Command
 
-    if (cmd === `${prefix}ping`){
-        return message.channel.send("pong")
-    }
-
-    if (cmd === `${prefix}serverinfo`){
-        let sEmbed = new Discord.MessageEmbed()
-        .setColor(colours.orange)
-        .setTitle("Server Info")
-        .setThumbnail(message.guild.iconURL)
-        .setAuthor(`${message.guild.name} Information`, message.guild.iconURL)
-        .addField("**Server Name:**", `${message.guild.name}`, true)
-        .addField("**Server Owner:**", `${message.guild.owner}`, true)
-        .addField("**Member Count:**", `${message.guild.memberCount}`, true)
-        .addField("**Role Count:**", `${message.guild.roles.count}`, true)
-        .setFooter(`SoupBot | created by txkedown | bincent#7777`, bot.user.displayAvatarURL);
-        message.channel.send({embed: sEmbed});
-
+    if (message.content.startsWith(prefix + "ping")) {
+        const startTime = Date.now();
+        message.channel.send(`Pong!`)
+        .then(msg => {
+            const endTime = Date.Now();
+            msg.edit(`Pong! (${endTime - startTime}ms)`);
+        });
     }
 })
+
+// Server and User Information Commands
+
+if (message.content.startsWith(prefix + "userinfo")) {
+    let user = message.mentions.users.first() || message.author;
+
+    let userinfo = {};
+    userinfo.avatar. = user.display.AvatarURL()
+    userinfo.name = user.username
+    userinfo.discrim - `#${user.discriminator}`;
+    userinfo.id = user.id;
+    userinfo.status = user.presence.status;
+    userinfo.registered = moment.utc(message.guild.members.get(user.id).user.createdAt).format("dddd, MMMM Do, YYYY");
+    userinfo.joined = moment.utc(message.guild.members.get(user.id).joinAt).format("dddd, MMMM Do, YYYY");
+
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(user.tag, userinfo.avatar)
+    .setThumbnail(userinfo.avatar)
+    .addField(`Username`, userinfo.name, true)
+    .addField(`Discriminator`, userinfo.discrim, true)
+    .addField(`ID`, userinfo.id, true)
+    .addField(`Status`, userinfo.status, true)
+    .addField(`Registered`, userinfo.registered, true)
+    .addField(`Joined`, userinfo.joined)
+
+    return message.channel.send(embed);
+}
+
+if (message.content.startsWith(prefix + "serverinfo")) {
+    const serverLevel = ["None", "Low", "Medium", "High", "Max"];
+    
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(message.guild.name, message.guild.iconURL())
+    .setThumbnail(message.guild.iconURL)
+    .addField(`Owner`, m.guild.owner.user.tag, true)
+    .addField(`ID`, message.guild.id, true)
+    .addField(`Members`, message.guild.memberCount, true)
+    .addField(`Bots`, message.guild.members.filter(mem => mem.user.bot === true).size, true)
+    .addField(`Online`, message.guild.members.filter(meme => mem.presence.status != "offline").size, true)
+    .addField(`Roles`, message.guild.roles.size, true)
+    .addField(`Verification Level`, serverLevel[m.guild.verificationLevel], true)
+    .addField(`Created Date`, moment.utc(message.guild.createdAt).format("dddd, MMMM Do, YYYY"), true)
+    .addField(`Role List [${message.guild.roles.size -1}]`, message.guild.roles.map(r => r).join(" ").replace("@everyone", " "))
+
+    return message.channel.send(embed);
+
+}
 
 bot.login(process.env.token);
